ce616b7deb3b0940439b8d6c36f1655edfef7bc0
Maxim Kolchin <kolchinmax@gmail.com>
2016-07-13 20:51:48 +0300

    Set Max-Age for observe resources

--- src/main/java/ru/semiot/platform/coap/BuildingObservationsResource.java
+++ src/main/java/ru/semiot/platform/coap/BuildingObservationsResource.java
@@ -3,14 +3,17 @@ package ru.semiot.platform.coap;
 import static org.eclipse.californium.core.coap.CoAP.ResponseCode.CONTENT;
 import static org.eclipse.californium.core.coap.MediaTypeRegistry.TEXT_PLAIN;
 
+import org.aeonbits.owner.ConfigFactory;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.server.resources.CoapExchange;
+import ru.semiot.platform.ServiceConfig;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class BuildingObservationsResource extends CoapResource implements Observer {
 
+  private static final ServiceConfig config = ConfigFactory.create(ServiceConfig.class);
   private String observations;
   private static final String name = "obs";
 
@@ -27,6 +30,10 @@ public class BuildingObservationsResource extends CoapResource implements Observ
 
   @Override
   public void handleGET(CoapExchange exchange) {
+    /**
+     * Details: https://github.com/eclipse/californium/issues/70
+     **/
+    exchange.setMaxAge(config.scheduledDelayObserve() * 10);
     exchange.respond(CONTENT, observations, TEXT_PLAIN);
   }
 
